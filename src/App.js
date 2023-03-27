89be0d4a7005a1199b8eba0ceec3c490e6a5390a
Marian4gc <mariangarcia1724@gmail.com>
2023-03-27 09:57:42 +0200

    primeras pruebas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
+import Cards from './components/Cards';
 import './App.css';
+import CardsB from './components/CardsB';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards />
+      {/* <CardsB /> */}
     </div>
   );
 }
